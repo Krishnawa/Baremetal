d98ebaed8ba07eb25dc561f9a8874bca5dc79f4a
Krishnawa <nadhkrishna@gmail.com>
2021-10-29 11:58:43 +0400

    small changes in the blink

--- AVR-Programming/blink/blink.c
+++ AVR-Programming/blink/blink.c
@@ -1,15 +1,19 @@
 #define F_CPU 16000000UL // CPU frequency
-#define LED_PIN 5 //Arduino BUILTIN_LED pin
 
 #include <avr/io.h>
 #include <util/delay.h>
 
-void main(void)
-{
-	DDRB |= (1 << LED_PIN); // set pin as output
-    while (1) // infinite loop
-    {
-		PORTB ^= (1 << LED_PIN); // toggles  pin		
-		_delay_ms(100); // busy wait, 100ms
-    }
+int main(void) {
+  // -------- Inits --------- //
+  DDRB |= (1 << PB5);           /* Data Direction Register B:
+                                   writing a one to the bit
+                                   enables output. */
+  // ------ Event loop ------ //
+  while (1) {
+    PORTB = 0b00100000;          /* Turn on 4th  bit/pin in PORTB */
+    _delay_ms(1000);                                           /* wait */
+    PORTB = 0b00000000;          /* Turn off all B pins, including LED */
+    _delay_ms(1000);                                           /* wait */
+  }                                                  /* End event loop */
+  return (0);
 }
