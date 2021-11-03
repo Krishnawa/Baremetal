e924c3453d6da7efeb17900b6303509d70be662e
Krishnawa <nadhkrishna@gmail.com>
2021-11-03 12:37:06 +0400

    Blink program using bitwise operator (&,^)

--- AVR-Programming/blink/blink.c
+++ AVR-Programming/blink/blink.c
@@ -2,7 +2,7 @@
 
 #include <avr/io.h>
 #include <util/delay.h>
-
+int8_t state = 0b00100000;
 int main(void) {
   // -------- Inits --------- //
   DDRB |= (1 << PB5);           /* Data Direction Register B:
@@ -10,9 +10,9 @@ int main(void) {
                                    enables output. */
   // ------ Event loop ------ //
   while (1) {
-    PORTB = 0b00100000;          /* Turn on 4th  bit/pin in PORTB */
-    _delay_ms(1000);                                           /* wait */
-    PORTB = 0b00000000;          /* Turn off all B pins, including LED */
+    PORTB = state;          /* Turn on 4th  bit/pin in PORTB */
+    _delay_ms(100);                                           /* wait */
+    PORTB ^= (1<<5);          /* Turn off all B pins, including LED */
     _delay_ms(1000);                                           /* wait */
   }                                                  /* End event loop */
   return (0);
